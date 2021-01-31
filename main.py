216ce12f3e8055535a6bdeca727217012fae704e
Harish Musti <68740895+hareshkm999@users.noreply.github.com>
2021-01-31 15:50:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -126,6 +126,6 @@ def predict():
 if __name__ == "__main__":
     #app.run(debug=True)
     host = '0.0.0.0'
-    port = 5012
+    port = 5000
     httpd = simple_server.make_server(host, port, app)
     httpd.serve_forever()
